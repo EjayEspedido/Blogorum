bbbf082660c5b3bd881355e7e40b7c67460014cf
EjayEspedido <eimanjohnespedido789@gmail.com>
2026-08-11 18:17:09 +0800

    Added netlify.toml netlify-build.sh

---
+++ netlify-build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+git clone https://github.com/flutter/flutter.git --depth 1 -b stable "$HOME/flutter"
+
+export PATH="$HOME/flutter/bin:$PATH"
+
+flutter --version
+flutter pub get
+flutter build web --release
